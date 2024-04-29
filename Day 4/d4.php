3167cfb00d303cdf5f7550a9cbe82406a8d161cd
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2024-04-29 13:36:06 +0300

    Add files via upload

---
+++ Day 4/d4.php
@@ -0,0 +1,32 @@
+<?php
+
+// intended comment
+// $x =1;
+// while($x<=5){
+//     echo 'the number is: ' . $x;
+//     echo '<br>';
+//     $x++;
+
+// }
+
+// echo '<br>';
+// echo 'here in for loop';
+// echo '<br>';
+
+for($i= 0; $i<=5; $i++){
+echo 'the number is: ' . $i;
+echo '<br>';
+
+}
+
+// $x=6;
+// do{
+//     echo 'the value of x is: ' .$x;
+//     $x++;
+//     echo '<br>';
+// }
+// while($x< 5);
+
+// echo 'end program';
+
+?>
---
+++ Day 4/loop.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Hover Rows</h2>
+  <p>The .table-hover class enables a hover state on table rows:</p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+
+    <!-- opening a new php tag -->
+    <?php
+    $x=1;
+    while($x<=5){
+    ?>  
+    //closing the php tag  
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+   <?php
+      $x++;
+    }
+    ?>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
