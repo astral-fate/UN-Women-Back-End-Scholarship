0582f9fc636540140e6a4a1b0fd3138baaf19cfa
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2024-06-04 06:53:25 +0300

    Add files via upload

---
+++ Day 13/Task 13/index.php
@@ -0,0 +1,63 @@
+
+<?php
+$title = 'ROYAL CARS - Car Rental HTML Template';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('admin/includes/head.php') ?>
+</head>
+
+<body>
+    <!-- Topbar Start -->
+    <?php include_once('admin/includes/topBar.php') ?>
+    <!-- Topbar End -->
+  
+    <!-- Navbar Start -->
+    <?php include_once('admin/includes/navBar.php') ?>
+    <!-- Navbar End -->
+
+    <!-- Search Start -->
+    <?php include_once('admin/includes/search.php') ?>
+    <!-- Search End -->
+    <!-- Carousel Start -->
+    <?php include_once('admin/includes/carousel.php') ?>
+    <!-- Carousel End -->
+    <!-- About Start -->
+    <?php include_once('admin/includes/about.php') ?>
+    <!-- About End -->
+    <!-- Services Start -->
+    <?php include_once('admin/includes/services.php') ?>
+    <!-- Services End -->
+    <!-- Banner Start -->
+    <?php include_once('admin/includes/banner.php') ?>
+    <!-- Banner End -->
+    <!-- Rent A Car Start -->
+    <?php include_once('admin/includes/rentCar.php') ?>
+    <!-- Rent A Car End -->
+    <!-- Team Start -->
+    <?php include_once('admin/includes/team.php') ?>
+    <!-- Team End -->
+    <!-- Banner Start -->
+    <?php include_once('admin/includes/banner2.php') ?>
+    <!-- Banner End -->
+    <!-- Testimonial Start -->
+    <?php include_once('admin/includes/testimonial.php') ?>
+    <!-- Testimonial End -->
+    <!-- Contact Start -->
+    <?php include_once('admin/includes/contact.php') ?>
+    <!-- Contact End -->
+    <!-- Vendor Start -->
+    <?php include_once('admin/includes/vendor.php') ?>
+    <!-- Vendor End -->
+    <!-- Footer Start -->
+    <?php include_once('admin/includes/footer.php') ?>
+    <!-- Footer End -->
+    <!-- Back to Top -->
+    <?php include_once('admin/includes/back.php') ?>
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
